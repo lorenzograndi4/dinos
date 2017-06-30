35ab8af6cd2ab89a74bd7649b791f73e63ddd9e3
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-06-30 13:26:38 +0200

    create dino model

---
+++ app/models/dinosaur.rb
@@ -0,0 +1,15 @@
+class Dinosaur < ApplicationRecord
+
+  # def self.order_by_name
+  #   order(:name)
+  # end
+
+  #OR
+
+  scope :order_by_name, -> { order(:name) }
+
+  def is_baby?
+    age < 3
+  end
+
+end
